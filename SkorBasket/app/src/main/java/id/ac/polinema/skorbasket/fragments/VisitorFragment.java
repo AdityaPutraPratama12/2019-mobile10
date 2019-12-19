a7790265a4a71c05f5e0ca5a4e7fef4ad41e804b
AdityaPutraPratama12 <aditp121998@gmail.com>
2019-12-19 18:51:15 +0700

    perubahan angka

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,6 +36,7 @@ public class VisitorFragment extends Fragment {
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 	}
 
 	@Override
@@ -52,7 +54,24 @@ public class VisitorFragment extends Fragment {
 		scoreVisitor = view.findViewById(R.id.scoreVisitor);
 		scoreSatuVisitor = view.findViewById(R.id.scoreSatuVisitor);
 
+		scoreDuaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+2);
+			}
+		});
 
-		// Tambahkan logic tombol di bagian bawah ini
+		scoreTigaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+3);
+			}
+		});
+
+		
+
+
+
+
 	}
 }
